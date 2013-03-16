66674c31c28ea38ff893444ad976cee1a53826d2
David Powell <djpowell@djpowell.net>
2013-03-16 10:54:35 +0000

    Initial commit

---
+++ src/net/djpowell/file_assoc_in.clj
@@ -0,0 +1,6 @@
+(ns net.djpowell.file-assoc-in)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/net/djpowell/file_assoc_in_test.clj
@@ -0,0 +1,7 @@
+(ns net.djpowell.file-assoc-in-test
+  (:use clojure.test
+        net.djpowell.file-assoc-in))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
