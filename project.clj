9af87f22411e94becc114c39e158a14ad95ef296
David Powell <djpowell@djpowell.net>
2013-03-23 17:41:52 +0000

    bump to 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.djpowell/file-assoc-in "0.1.0-SNAPSHOT"
+(defproject net.djpowell/file-assoc-in "0.1.0"
   :description "Utility for updating clojure data files containing maps (such as leiningen profiles), without losing comments and lexical layout"
   :url "https://github.com/djpowell/file-assoc-in"
   :license {:name "Eclipse Public License"
